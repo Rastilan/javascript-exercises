cfc869f8a9bcf6c52ed20885037ef08a76f72c7e
Rastilan <christoperbhall@gmail.com>
2022-01-10 01:46:15 -0500

    complete, no removed scripts

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+let num = 0;
+let answer = 0;
+const ftoc = function(F) {
+  num = (F - 32) * 0.5555;
+  return answer = Math.round(num * 10) / 10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  num = C * 1.8 + 32;
+  return answer = Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
