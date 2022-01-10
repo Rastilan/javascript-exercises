59a1f3d16ca250d4bb4363744f6bf792192b7bba
Rastilan <christoperbhall@gmail.com>
2022-01-10 01:17:42 -0500

    complete. No removing scripts

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let answer = 0;
+    if (start < 0 || end < 0 || typeof start !== "number" || typeof end !== "number"){
+        return "ERROR";
+    }
+    else if(start > end){
+        for(i = end; i <= start; i++){
+            answer = answer + i;
+        }
+    }
+    else if (end > start){
+        for(i = start; i <= end; i++){
+            answer = answer + i;
+        }
+    }
 
+    return answer;
 };
 
 // Do not edit below this line
