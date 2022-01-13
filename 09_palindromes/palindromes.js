beaf8dfad671a2ec8c5af91445e4e0603e0d4cb8
Rastilan <christoperbhall@gmail.com>
2022-01-13 02:48:17 -0500

    complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = "";
+    orig = str.toLowerCase().replace(/\W/g, "");
+    newStr = orig.split("").reverse().join("");
+    return (newStr === orig);
 };
 
 // Do not edit below this line
