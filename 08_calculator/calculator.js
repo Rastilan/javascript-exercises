789a2afbe67aa8bfbea7ac5035f4a862f8406b20
Rastilan <christoperbhall@gmail.com>
2022-01-13 02:11:26 -0500

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.length ? arr.reduce((curVal, prevVal) => prevVal + curVal) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length ? arr.reduce((curVal, prevVal) => prevVal * curVal) : 0;
 };
 
-const power = function() {
-	
+const power = function(numVal, powerVal) {
+  let answer = 1;
+	for(let i = 0; i < powerVal; i++){
+    answer = answer * numVal;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  let answer = val;
+  let math = val - 1;
+	for(let i = 0; i < math; i++){
+    val--;
+    answer *= val;
+  } 
+  if(answer < 1){ answer = 1; }
+  return answer;
 };
 
 // Do not edit below this line
