a39f3af8873f205700e3a66ad1ce759a27e6ab73
Rastilan <christoperbhall@gmail.com>
2022-01-13 03:10:07 -0500

    complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+prev = 0;
+next = 1;
+if(val < 0) { return "OOPS"; }
+    for(i=0; i < val; i++){
+        next = prev + next;
+        prev = next - prev;
+    }
+    return prev;
 };
 
 // Do not edit below this line
