d8e19664cdfd7e571519485f037ffd5811791f7d
Rastilan <christoperbhall@gmail.com>
2022-01-14 16:17:43 -0500

    complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(x) {
 
-};
+    const oldest = x.reduce(function(per1, per2) {
+        
+        if(!per1.yearOfDeath){
+            per1.yearOfDeath = new Date().getFullYear();
+        }
+        else if(!per2.yearOfDeath){
+            per2.yearOfDeath = new Date().getFullYear();
+        }
+        const person1 = per1.yearOfDeath - per1.yearOfBirth;
+        const person2 = per2.yearOfDeath - per2.yearOfBirth;
+        if(person1 > person2){
+            
+            return(per1);
+        }
+        else { 
 
+            return(per2);
+        }
+            
+
+
+    });
+        
+
+
+    return oldest;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
