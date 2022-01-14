9879236aff6f972cc862e2f236442b5f508339e2
Rastilan <christoperbhall@gmail.com>
2022-01-14 03:59:49 -0500

    todo

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(x) {
 
+    const newArr = x.map(arr => {arr.title});
+    // let newArr = [];
+    // for(let i = 0; i < x.length; i++){       
+    //     newArr.push(x[i].title);
+
+    // }
+    console.log(newArr);
+    return newArr;
 };
 
 // Do not edit below this line
